42c6fb04bfa23d4d6961ac3653e31b26ed2ab87c
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2024-03-23 10:41:06 +0000

    Correctly override config

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    close_if_last_window = true,
+    -- enable_diagnostics = false,
+    -- enable_git_status = false,
+    -- git_status_async = false,
+    -- log_to_file = true,
+  },
+}
