79b03b240fa0ac6c8d591f78b7f3c35f1fa51f4e
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2024-08-04 03:22:00 +0100

    Adjust remote-nvim config to launch new tmux window on attach

--- lua/plugins/remote-nvim.lua
+++ lua/plugins/remote-nvim.lua
@@ -7,4 +7,16 @@ return {
     "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
   },
   config = true,
+  opts = {
+    client_callback = function(port, workspace_config)
+      local cmd = ("tmux new-window -n 'nvim-remote'  'nvim --server localhost:%s --remote-ui'"):format(port)
+      vim.fn.jobstart(cmd, {
+        detach = true,
+        on_exit = function(job_id, exit_code, event_type)
+          -- This function will be called when the job exits
+          print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+        end,
+      })
+    end,
+  },
 }
