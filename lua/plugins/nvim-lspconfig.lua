25536889a4e84111a69eacbfcb5c1e883975ca34
camin-mccluskey <mccluskey.camin@gmail.com>
2024-10-07 13:43:31 +0100

    Swap back to vtsls with eslint pinned to 4.8.0 (unfortunately the later is not configurable)

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,25 +4,25 @@ return {
   "neovim/nvim-lspconfig",
   version = "*",
   opts = {
-    servers = {
-      eslint = {
-        root_dir = util.root_pattern(
-          "eslint.config.js",
-          "eslint.config.cjs",
-          "eslint.config.mjs",
-          "eslint.config.ts",
-          "eslint.config.cts",
-          "eslint.config.mts",
-          ".eslintrc",
-          ".eslintrc.js",
-          ".eslintrc.cjs",
-          ".eslintrc.mjs"
-        ),
-      },
-      biome = {
-        root_dir = util.root_pattern("biome.json", "biome.jsonc"),
-        single_file_support = false,
-      },
-    },
+    -- servers = {
+    --   eslint = {
+    --     root_dir = util.root_pattern(
+    --       "eslint.config.js",
+    --       "eslint.config.cjs",
+    --       "eslint.config.mjs",
+    --       "eslint.config.ts",
+    --       "eslint.config.cts",
+    --       "eslint.config.mts",
+    --       ".eslintrc",
+    --       ".eslintrc.js",
+    --       ".eslintrc.cjs",
+    --       ".eslintrc.mjs"
+    --     ),
+    --   },
+    --   biome = {
+    --     root_dir = util.root_pattern("biome.json", "biome.jsonc"),
+    --     single_file_support = false,
+    --   },
+    -- },
   },
 }
