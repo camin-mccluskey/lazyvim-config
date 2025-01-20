94f1b0a21a72168319272843d0b01caefdafe4ec
camin-mccluskey <mccluskey.camin@gmail.com>
2025-01-20 16:13:32 +0000

    Use default Anthropic endpoint for completion

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,45 +3,7 @@ return {
   event = "VeryLazy",
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  opts = {
-    provider = "ollama",
-    vendors = {
-      ---@type AvanteProvider
-      ollama = {
-        ["local"] = true,
-        endpoint = "127.0.0.1:11434/v1",
-        model = "deepseek-coder-v2:16b",
-        parse_response_data = function(data_stream, event_state, opts)
-          require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
-        end,
-        parse_curl_args = function(opts, code_opts)
-          return {
-            url = opts.endpoint .. "/chat/completions",
-            headers = {
-              ["Accept"] = "application/json",
-              ["Content-Type"] = "application/json",
-            },
-            body = {
-              model = opts.model,
-              messages = require("avante.providers").copilot.parse_messages(code_opts),
-              max_tokens = 2048,
-              stream = true,
-            },
-          }
-        end,
-      },
-    },
-    -- providers = {
-    --   {
-    --     name = "deepseek",
-    --     type = "ollama",
-    --     model = "deepseek-coder-v2:16b",
-    --     -- You can also use other Deepseek models like:
-    --     -- model = "deepseek-coder:33b"
-    --     -- model = "deepseek-llm"
-    --   },
-    -- },
-  },
+  opts = {},
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
