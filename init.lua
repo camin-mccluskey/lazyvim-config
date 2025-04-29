b84690f5027f69d48ec15bd92bdf7b3ee1d60dad
camin-mccluskey <mccluskey.camin@gmail.com>
2025-04-30 00:29:28 +0100

    Add macro for console log

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-------
+--- Custom Macros ---
+-------
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la" .. esc .. "pl")
